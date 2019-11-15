7b378deae740aa00940e283e0779998d6a7bfae4
Peter Hillyard <peterhillyard@gmail.com>
2019-11-14 23:20:30 -0700

    Adding constants used in ethernet listener

---
+++ homenet/const.py
@@ -0,0 +1,12 @@
+ethernet_header_parts = [
+    'dest_mac_as_bytes',
+    'src_mac_as_bytes',
+    'eth_type_as_bytes',
+    'payload_as_bytes',
+]
+
+# Ethernet header format is:
+# - 6 bytes for destination mac
+# - 6 bytes for source mac
+# - 2 bytes for ethernet packet type
+ethernet_header_fmt = '!6s6s2s'
