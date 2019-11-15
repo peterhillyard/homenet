12f46cf65b07361d0b0655ea94d9322b2b7b9c6e
Peter Hillyard <peterhillyard@gmail.com>
2019-11-15 16:28:26 -0700

    Adding the smart arp listener and sender

---
+++ homenet/smartarp.py
@@ -0,0 +1,112 @@
+import arppy as arppy
+import comms as comms
+import time as time
+
+
+class SmartARPListener(arppy.ARPListener):
+
+    def __init__(self, sys_settings_fname=None):
+        super().__init__(sys_settings_fname)
+        self.sub_list = [b'new_table']
+        self.comms = comms.Comms(sys_settings_fname)
+        self.comms.set_subscriptions(self.sub_list)
+
+    def smart_arp_listen_routine(self):
+        # Get an ethernet packet
+        self.recv_ethernet_packet()
+
+        # Send out arp data if it was an arp packet
+        if self.is_arp_packet():
+            self.unpack_eth_payload_into_arp_data()
+            self.comms.send_msg('new_arp_pkt', self.get_styled_arp_data())
+
+        # Check if a new mac table was sent
+        msg = self.comms.recv_msg()
+        if msg:
+            self.replace_old_table(msg)
+
+    def replace_old_table(self, msg):
+        pass
+
+    def clean_up(self):
+        super().clean_up()
+        self.comms.close_pub_sub()
+
+
+class SmartARPSender(arppy.ARPSender):
+
+    def __init__(self, sys_settings_fname=None):
+        super().__init__(sys_settings_fname)
+        self.sub_list = [b'new_table']
+        self.comms = comms.Comms(sys_settings_fname)
+        self.comms.set_subscriptions(self.sub_list)
+        self.broadcast_interval = 60.0
+        self.num_direct_between_broadcasts = 2
+        self.direct_interval = self.broadcast_interval / (self.num_direct_between_broadcasts + 1)
+
+        self.prev_broadcast_time = 0
+        self.prev_direct_arp_time = 0
+
+    def smart_arp_send_routine(self):
+        cur_time = time.time()
+
+        # Periodically send a broadcast to update IPs
+        # and find new MACs
+        if cur_time - self.prev_broadcast_time > 30.0:
+            print('broadcast', cur_time)
+            self.prev_broadcast_time = cur_time
+            self.send_broadcast()
+
+        # Periodically send direct arp requests to
+        # each mac/ip pair in the table
+        if (cur_time - self.prev_direct_arp_time > self.direct_interval) and \
+           (cur_time - self.prev_broadcast_time > self.direct_interval):
+            print('direct', cur_time)
+            self.prev_direct_arp_time = cur_time
+
+        # Check for new mac tables
+        msg = self.comms.recv_msg()
+        if msg:
+            self.replace_old_table(msg)
+
+    def send_broadcast(self):
+        ip_addr = self.net_interface.get_interface_ip('str_dots')
+        ip_hdr = '.'.join(ip_addr.split('.')[:-1])
+
+        for ii in range(256):
+            dst_ip = ip_hdr + '.{}'.format(ii)
+            self.send_arp_request(dst_ip)
+            time.sleep(0.006)
+
+    def replace_old_table(self, msg):
+        pass
+
+    def clean_up(self):
+        super().clean_up()
+        self.comms.close_pub_sub()
+
+
+if __name__ == '__main__':
+    import sys
+    sys_settings_fname = 'sys_settings.json'
+
+    if 'listen' in sys.argv:
+        listener = SmartARPListener(sys_settings_fname)
+
+        is_running = True
+        while is_running:
+            try:
+                listener.smart_arp_listen_routine()
+            except KeyboardInterrupt:
+                is_running = False
+                listener.clean_up()
+    elif 'send' in sys.argv:
+        sender = SmartARPSender(sys_settings_fname)
+
+        is_running = True
+        while is_running:
+            try:
+                sender.smart_arp_send_routine()
+            except KeyboardInterrupt:
+                is_running = False
+                sender.clean_up()
