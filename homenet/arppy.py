f657ca5765df3ab72564305488ba0cb6c4e17ef3
Peter Hillyard <peterhillyard@gmail.com>
2019-11-15 02:59:52 -0700

    Added an arp sender
    The sender can send a broadcast or a direct request

--- homenet/arppy.py
+++ homenet/arppy.py
@@ -34,6 +34,59 @@ class ARPListener(ethpy.EthernetListener):
             self.arp_data[c.arp_packet_parts[ii]] = tmp[ii]
 
 
+class ARPSender(ethpy.EthernetSender):
+
+    def __init__(self, iface_name=None):
+        super().__init__(iface_name)
+
+        self.init_some_bytes()
+
+    def init_some_bytes(self):
+        tmp = self.mac_as_bytes + c.arp_eth_type
+        self.const_eth_hdr_part_as_bytes = tmp
+
+        tmp = b''
+        for key in c.arp_packet_const_names:
+            tmp += c.arp_packet_consts[key]
+        tmp += self.mac_as_bytes
+        tmp += self.ip_as_bytes
+        self.const_arp_part_as_bytes = tmp
+
+    def send_arp_request(self, dest_ip_with_dots, dest_hw_with_colons=None):
+        trgt_ip_as_bytes = self.convert_ip_with_dots_to_bytes(dest_ip_with_dots)
+        arp_bytes = self.const_arp_part_as_bytes
+
+        if dest_hw_with_colons:
+            eth_bytes = self.convert_mac_with_colon_to_bytes(dest_hw_with_colons)
+            arp_bytes += dest_mac_as_bytes
+        else:
+            eth_bytes = c.arp_broadcast_eth_dest_mac
+            arp_bytes += c.arp_broadcast_arp_trgt_mac
+
+        eth_bytes += self.const_eth_hdr_part_as_bytes
+        arp_bytes += trgt_ip_as_bytes
+
+        self.send_frame(eth_bytes, arp_bytes)
+
+
+def arp_sender_main():
+    import time as time
+    sender = ARPSender()
+
+    prev_time = time.time()
+    is_running = True
+    while is_running:
+        try:
+            cur_time = time.time()
+            if cur_time - prev_time > 3:
+                prev_time = cur_time
+                sender.send_arp_request('10.0.0.7')
+                print('msg sent', cur_time)
+        except KeyboardInterrupt:
+            is_running = False
+            sender.close_socket()
+
+
 def arp_listener_main():
     listener = ARPListener()
 
@@ -49,4 +102,9 @@ def arp_listener_main():
 
 
 if __name__ == '__main__':
-    arp_listener_main()
+    import sys
+
+    if 'listen' in sys.argv:
+        arp_listener_main()
+    elif 'send' in sys.argv:
+        arp_sender_main()
