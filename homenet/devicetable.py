8107d0592d2e7069b2bd218c2916f530dc471cae
Peter Hillyard <peterhillyard@gmail.com>
2019-11-27 22:56:42 -0700

    Device table send new table in new format
    We removed the alias and the last seen from the device table

--- homenet/devicetable.py
+++ homenet/devicetable.py
@@ -2,7 +2,6 @@ import comms as comms
 import json as json
 import uuid as uuid
 import time as time
-import datetime as datetime
 
 
 class DeviceTable:
@@ -30,32 +29,57 @@ class DeviceTable:
         src_ip = payload['sender_ip_as_str_with_dots']
 
         pub_new_table_flag = False
-        utc_now = datetime.datetime.utcnow()
         if src_mac in self.device_lut.keys():
-            dev_ip = self.device_lut[src_mac]['ip']
-            if dev_ip != src_ip:
+            device_ip = self.device_lut[src_mac]['ip']
+            if device_ip != src_ip:
                 pub_new_table_flag = True
                 self.device_lut[src_mac]['ip'] = src_ip
-            self.device_lut[src_mac]['last_seen'] = \
-                utc_now.isoformat() + '+00:00'
         else:
             new_device = {
                 'id': str(uuid.uuid4()),
                 'mac': src_mac,
                 'ip': src_ip,
-                'alias': '',
-                'last_seen': utc_now.isoformat() + '+00:00'
             }
             self.device_list.append(new_device)
             self.device_lut = update_device_lut(self.device_list)
             pub_new_table_flag = True
 
-        # TODO: save data to database
-        save_device_table(self.device_table_fname, self.device_list)
-
         if pub_new_table_flag:
             self.comms.send_msg('new_table', self.device_list)
             # TODO: publish new table
+
+        # payload = json.loads(msg[1].decode('utf-8'))
+
+        # src_mac = payload['sender_mac_as_str_with_colons']
+        # src_ip = payload['sender_ip_as_str_with_dots']
+
+        # pub_new_table_flag = False
+        # utc_now = datetime.datetime.utcnow()
+        # if src_mac in self.device_lut.keys():
+        #     device_ip = self.device_lut[src_mac]['ip']
+        #     if device_ip != src_ip:
+        #         pub_new_table_flag = True
+        #         self.device_lut[src_mac]['ip'] = src_ip
+        #     self.device_lut[src_mac]['last_seen'] = \
+        #         utc_now.isoformat() + '+00:00'
+        # else:
+        #     new_device = {
+        #         'id': str(uuid.uuid4()),
+        #         'mac': src_mac,
+        #         'ip': src_ip,
+        #         'alias': '',
+        #         'last_seen': utc_now.isoformat() + '+00:00'
+        #     }
+        #     self.device_list.append(new_device)
+        #     self.device_lut = update_device_lut(self.device_list)
+        #     pub_new_table_flag = True
+
+        # # TODO: save data to database
+        # save_device_table(self.device_table_fname, self.device_list)
+
+        # if pub_new_table_flag:
+        #     self.comms.send_msg('new_table', self.device_list)
+        #     # TODO: publish new table
 
     def clean_up(self):
         self.comms.close_pub_sub()
